d053d373557a390affc43c4ee282c0cc13b99153
Ceratosenzaf <ceratosenzaf@gmail.com>
2024-03-08 16:50:09 +0100

    fix: bubble sort

--- src/hooks/sorting-algos/bubble-sort.ts
+++ src/hooks/sorting-algos/bubble-sort.ts
@@ -3,32 +3,33 @@
 	Time complexity: O(n^2)
 	Space complexity: O(1)
 	
-	for i in [0, n-1]:
-		for j in [0, n-1]:
-			if array[j] > array[j+1]:
-				swap(array[j], array[j+1])
+	for i in [0, n-2]:
+		for j in [i, n-1]:
+			if array[i] > array[j]:
+				swap(array[i], array[j])
 	return array
 */
 
 import { get } from 'svelte/store'
 import type { SortingAlgoRithm } from '../use-sorting'
 import { sortedArray } from '../../stores/sorted-array'
-import { selectedIndex } from '../../stores/selected-index'
+import { otherIndexes, selectedIndex } from '../../stores/selected-index'
 import { isPlaying } from '../../stores/playing'
 import { sleep } from '../../utils/sleep'
 
 export const bubbleSort: SortingAlgoRithm = async (initialArray, sleepTime = 0) => {
-	for (let i = 0; i < initialArray.length; i++) {
+	for (let i = 0; i < initialArray.length - 1; i++) {
 		let swapped = false
-		for (let j = 0; j < initialArray.length - 1; j++) {
+		selectedIndex.set(i)
+
+		for (let j = i + 1; j < initialArray.length; j++) {
 			const currentArray = get(sortedArray)
 			if (!get(isPlaying)) return currentArray
 
-			selectedIndex.set(j)
+			otherIndexes.set([j])
 
-			if (currentArray[j] > currentArray[j + 1]) {
-				sortedArray.swapAtIndexes(j, j + 1)
-				selectedIndex.set(j + 1)
+			if (currentArray[i] > currentArray[j]) {
+				sortedArray.swapAtIndexes(i, j)
 				swapped = true
 			}
 
